47758f1e947787031b89de97bd583a61ecd69962
Nelgit007 <osagienelson24@gmail.com>
2023-09-04 16:03:21 +0100

    updated python app

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # cloud-native-monitoring-web-app
-A simple cloud monitoring web application built in python-flask
+A simple cloud monitoring web application built in python-flask.
+## Install Python 3.X
+## Create Python Virtual Environment
+## Write cLoud-native Flask App
+## Jinja templating for Dynamic Web Pages
+## Used Pip to Freeze Python Dependencies
+## Build docker image using Dockerfile
+## Write Docker Compose file
+## Write Kubernetes Manifest files for the application
--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, render_template
-import socket, psutil
+import socket
 
 app = Flask(__name__)
 
@@ -22,21 +22,11 @@ def get_host_details():
     ip_address = socket.gethostbyname(hostname)
     return str(hostname), str(ip_address)
 
-#Function that get system cpu and memory usage
-def app_health():
-    cpu_percent = psutil.cpu_percent()
-    mem_percent = psutil.virtual_memory().percent
-    message = None
-    if cpu_percent > 85 or mem_percent > 70:
-        message = "High CPU and Memory utilization, scale up"
-    return str(cpu_percent), (mem_percent)
-
 @app.route("/details")
 def details():
-    cpu_percent, mem_percent = app_health()
     hostname, ip_address = get_host_details()
-    return render_template('index.html', HOSTNAME=hostname, IP=ip_address, CPU=cpu_percent, MEM=mem_percent)
+    return render_template('index.html', HOSTNAME=hostname, IP=ip_address)
 
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
