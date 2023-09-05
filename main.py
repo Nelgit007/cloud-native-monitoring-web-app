6aed02edcd4b7c7d8fc4d59a699d365ce0272f13
Nelgit007 <osagienelson24@gmail.com>
2023-09-05 21:55:44 +0100

    updated file dir & config file

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, render_template
+import socket
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, I am Nelson Osagie, a Cloud / DevOps Engineer.</p>"
+
+
+# App health endpoint
+@app.route("/status")
+def app_status():
+    return jsonify(
+        status="RUNNING"
+    )
+
+
+# Function for fetching host name and ip add
+def get_host_details():
+    # Get the hostname
+    hostname = socket.gethostname()
+    # Get the IP address
+    ip_address = socket.gethostbyname(hostname)
+    return str(hostname), str(ip_address)
+
+
+@app.route("/details")
+def details():
+    hostname, ip_address = get_host_details()
+    return render_template('index.html', HOSTNAME=hostname, IP=ip_address)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
